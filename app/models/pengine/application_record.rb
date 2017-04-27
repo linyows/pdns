4058d9492ef7959910d9be7b8048b8e12478a4fb
linyows <linyows@gmail.com>
2017-04-27 17:28:15 +0900

    use other database

--- app/models/pengine/application_record.rb
+++ app/models/pengine/application_record.rb
@@ -1,6 +1,11 @@
 module Pengine
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
-    DOMAIN_FORMAT = /\A[^-][a-z0-9\-\._]*[^-]\.[a-z0-9-]{2,}\Z/
+
+    f = Rails.root.join('config',
+      "database-#{Pengine.database_config_file_suffix}.yml")
+    @_conf ||= ::YAML::load(ERB.new(IO.read(f.to_s)).result)[Rails.env.to_s]
+
+    establish_connection @_conf
   end
 end
