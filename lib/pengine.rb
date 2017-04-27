e87381935cfded3247ea594541931a04dde28136
linyows <linyows@gmail.com>
2017-04-27 17:21:15 +0900

    add config

--- lib/pengine.rb
+++ lib/pengine.rb
@@ -1,5 +1,16 @@
-require "pengine/engine"
+require 'pengine/engine'
 
 module Pengine
-  # Your code goes here...
+  mattr_accessor :domain_format
+  self.domain_format = /\A[^-][a-z0-9\-\._]*[^-]\.[a-z0-9-]{2,}\Z/
+
+  mattr_accessor :domain_format_for_routes
+  self.domain_format_for_routes = /[a-z0-9\-\._]*\.[a-z0-9-]{2,}/
+
+  mattr_accessor :database_config_file_suffix
+  self.database_config_file_suffix = :pdns
+
+  def self.setup
+    yield self
+  end
 end
