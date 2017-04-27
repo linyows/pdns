68a8b1184bc02632a3c0b4dd766af3fdbff6cb9b
linyows <linyows@gmail.com>
2017-04-27 17:21:41 +0900

    add templates

---
+++ lib/generators/templates/pengine.rb
@@ -0,0 +1,6 @@
+Pengine.setup do |c|
+  # Defaults:
+  # c.domain_format = /\A[^-][a-z0-9\-\._]*[^-]\.[a-z0-9-]{2,}\Z/
+  # c.domain_format_for_routes = /[a-z0-9\-\._]*\.[a-z0-9-]{2,}/
+  # c.database_config_file_suffix = :pdns
+end
