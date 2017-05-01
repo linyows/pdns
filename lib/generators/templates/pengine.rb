98475decaab0d065f61c37e1b032a7a3fb70e9a9
linyows <linyows@gmail.com>
2017-05-01 16:00:26 +0900

    add to generator

--- lib/generators/templates/pengine.rb
+++ lib/generators/templates/pengine.rb
@@ -1,6 +1,9 @@
 Pengine.setup do |c|
   # Defaults:
-  # c.domain_format = /\A[^-][a-z0-9\-\._]*[^-]\.[a-z0-9-]{2,}\Z/
-  # c.domain_format_for_routes = /[a-z0-9\-\._]*\.[a-z0-9-]{2,}/
+  # c.domain_format               = /\A[^-][a-z0-9\-\._]*[^-]\.[a-z0-9-]{2,}\Z/
+  # c.domain_format_for_routes    = /[a-z0-9\-\._]*\.[a-z0-9-]{2,}/
   # c.database_config_file_suffix = :pdns
+  # c.db_name                     = :pdns
+  # c.db_conf_path                = "config/database-#{self.db_name}.yml"
+  # c.db_dir_path                 = File.expand_path('../../db', __FILE__)
 end
