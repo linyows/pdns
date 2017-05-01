b177924bae620300c1824ffeb0b4ad9f7d03221b
linyows <linyows@gmail.com>
2017-05-01 16:31:48 +0900

    use method in pengine

--- app/models/pengine/application_record.rb
+++ app/models/pengine/application_record.rb
@@ -1,11 +1,6 @@
 module Pengine
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
-
-    f = Rails.root.join('config',
-      "database-#{Pengine.database_config_file_suffix}.yml")
-    @_conf ||= ::YAML::load(ERB.new(IO.read(f.to_s)).result)[Rails.env.to_s]
-
-    establish_connection @_conf
+    establish_connection Pengine.db_conf[Rails.env.to_s]
   end
 end
