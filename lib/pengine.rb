8af600cc7b827a475f198b1ce96e8a5beec790e1
linyows <linyows@gmail.com>
2017-05-01 16:00:38 +0900

    remove unused config

--- lib/pengine.rb
+++ lib/pengine.rb
@@ -7,9 +7,6 @@ module Pengine
   mattr_accessor :domain_format_for_routes
   self.domain_format_for_routes = /[a-z0-9\-\._]*\.[a-z0-9-]{2,}/
 
-  mattr_accessor :database_config_file_suffix
-  self.database_config_file_suffix = :pdns
-
   mattr_accessor :db_name
   self.db_name = :pdns
 
