08e0f4f11363676fbd2bb72a187c8f35daa4c85b
linyows <linyows@gmail.com>
2017-05-01 16:32:39 +0900

    use database.yml

--- lib/generators/pengine/install_generator.rb
+++ lib/generators/pengine/install_generator.rb
@@ -1,12 +1,12 @@
 module Pengine
   class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path("../../templates", __FILE__)
+    source_root File.expand_path('../../templates', __FILE__)
 
     desc 'Creates a Pengine initializer.'
 
     def copy_initializer
       template 'pengine.rb', 'config/initializers/pengine.rb'
-      template 'database-pdns.yml', 'config/database-pdns.yml'
+      template 'database.yml', "config/database-#{Pengine.db_name}.yml"
     end
   end
 end
