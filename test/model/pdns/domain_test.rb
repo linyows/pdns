e92e350a1451082cd5e62d05825b18240fbaf9f8
linyows <linyows@gmail.com>
2017-05-02 22:58:03 +0900

    simplize test

--- test/model/pdns/domain_test.rb
+++ test/model/pdns/domain_test.rb
@@ -2,70 +2,50 @@ require 'test_helper'
 
 module PDNS
   class DomainTest < ActiveSupport::TestCase
-    test 'valid with a name, type' do
-      domain = Domain.new(name: 'example.com', type: 'NATIVE')
-      assert domain.valid?
+    setup do
+      @domain = Domain.new(name: 'example.com', type: 'NATIVE')
     end
 
-    test 'invalid without a name' do
-      domain = Domain.new(type: 'NATIVE')
+    test 'domain valid' do
+      assert @domain.valid?
+    end
+
+    test 'domain invalid' do
+      domain = Domain.new
       domain.valid?
+
+      assert_equal domain.errors.count, 2
       assert_includes domain.errors[:name], 'can\'t be blank'
-    end
-
-    test 'valid without a type' do
-      domain = Domain.new(name: 'example.com')
-      assert domain.valid?
-    end
-
-    test 'valid when name is example-0123456789.com' do
-      domain = Domain.new(name: 'example-0123456789.com')
-      assert domain.valid?
-    end
-
-    test 'valid when name is xn--vck8cuc4a.jp' do
-      domain = Domain.new(name: 'xn--vck8cuc4a.jp')
-      assert domain.valid?
-    end
-
-    test 'valid when name is example.co.jp' do
-      domain = Domain.new(name: 'example.co.jp')
-      assert domain.valid?
-    end
-
-    test 'invalid when name is example-.com' do
-      domain = Domain.new(name: 'example-.com')
-      domain.valid?
       assert_includes domain.errors[:name], 'is invalid'
     end
 
-    test 'invalid when name is -example.com' do
-      domain = Domain.new(name: '-example.com')
-      domain.valid?
-      assert_includes domain.errors[:name], 'is invalid'
-    end
+    test 'domain name validation' do
+      @domain.name = 'example-0123456789.com'
+      assert @domain.valid?
 
-    test 'invalid when name is サンプル.jp' do
-      domain = Domain.new(name: 'サンプル.jp')
-      domain.valid?
-      assert_includes domain.errors[:name], 'is invalid'
-    end
+      @domain.name = 'xn--vck8cuc4a.jp'
+      assert @domain.valid?
 
-    test 'invalid when same record is registered' do
-      Domain.new(name: 'example.com').save
+      @domain.name = 'example.co.jp'
+      assert @domain.valid?
+
+      @domain.name = 'example-.com'
+      assert_not @domain.valid?
+
+      @domain.name = 'example.xn--tckwe'
+      assert @domain.valid?
+
+      @domain.name = 'xn--vck8cuc4a.xn--tckwe'
+      assert @domain.valid?
+
+      @domain.name = 'サンプル.jp'
+      assert_not @domain.valid?
+
+      @domain.name = 'example.com'
+      @domain.save
       domain = Domain.new(name: 'example.com', type: 'NATIVE')
       domain.valid?
       assert_includes domain.errors[:name], 'has already been taken'
-    end
-
-    test 'valid when name is example.xn--tckwe' do
-      domain = Domain.new(name: 'example.xn--tckwe')
-      assert domain.valid?
-    end
-
-    test 'valid when name is xn--vck8cuc4a.xn--tckwe' do
-      domain = Domain.new(name: 'xn--vck8cuc4a.xn--tckwe')
-      assert domain.valid?
     end
   end
 end
