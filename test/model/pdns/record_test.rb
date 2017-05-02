d1f27b06c2dc4d928464bfdfc98e31c37764cde6
linyows <linyows@gmail.com>
2017-05-02 22:46:22 +0900

    add test for record

--- test/model/pdns/record_test.rb
+++ test/model/pdns/record_test.rb
@@ -39,5 +39,97 @@ module PDNS
 
       assert_equal record.errors.count, 9
     end
+
+    test 'record name validation' do
+      @record.name = 'example-0123456789.com'
+      assert @record.valid?
+
+      @record.name = 'xn--vck8cuc4a.jp'
+      assert @record.valid?
+
+      @record.name = 'example.co.jp'
+      assert @record.valid?
+
+      @record.name = 'www.example.com'
+      assert @record.valid?
+
+      @record.name = 'example-.com'
+      assert_not @record.valid?
+
+      @record.name = '-example.com'
+      assert_not @record.valid?
+
+      @record.name = 'サンプル.jp'
+      assert_not @record.valid?
+
+      @record.name = 'ho_ge.foo.com'
+      assert @record.valid?
+
+      @record.name = 'example.xn--tckwe'
+      assert @record.valid?
+
+      @record.name = 'xn--vck8cuc4a.xn--tckwe'
+      assert @record.valid?
+
+      @record.name = 'www.example.xn--tckwe'
+      assert @record.valid?
+
+      @record.name = 'foo.com'
+      @record.save
+      copy = @record.dup
+      assert_not copy.valid?
+      assert_includes copy.errors[:name], 'has already been taken'
+    end
+
+    test 'record ttl validation' do
+      @record.ttl = 'hoge'
+      assert_not @record.valid?
+    end
+
+    test 'record prio validation' do
+      @record.type = 'A'
+      @record.prio = 'hoge'
+      assert @record.valid?
+
+      @record.type = 'MX'
+      @record.prio = 'hoge'
+      assert_not @record.valid?
+    end
+
+    test 'record type validation' do
+      @record.type = 'A'
+      @record.prio = nil
+      assert @record.valid?
+
+      @record.type = 'MX'
+      @record.prio = nil
+      assert_not @record.valid?
+    end
+
+    test 'record before_update for change_date' do
+      @record.save
+      assert @record.change_date == nil
+      @record.update_attribute(:ttl, 30)
+      assert @record.change_date > 0
+    end
+
+    test 'record update_serial for soa' do
+      src = 'foo.jp. bar.com. 1 7200 900 1209600 86400'
+      dsc = 'foo.jp. bar.com. 2 7200 900 1209600 86400'
+      soa = Record.new(
+        domain_id: @domain.id,
+        name: @domain.name,
+        type: 'SOA',
+        content: src,
+        ttl: 600
+      )
+      soa.save
+      assert_equal src, soa.content
+
+      @record.save
+      @record.update_serial
+      soa.reload.content
+      assert_equal dsc, soa.content
+    end
   end
 end
