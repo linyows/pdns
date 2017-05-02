e9a42f93ae41d1f4fc7f1a466f9f766e9abbecfd
linyows <linyows@gmail.com>
2017-05-03 00:27:42 +0900

    add controller tests

---
+++ test/controller/pdns/domains_controller_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module PDNS
+  class DomainsControllerTest < ActionController::TestCase
+  end
+end
---
+++ test/controller/pdns/records_controller_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module PDNS
+  class RecordsControllerTest < ActionController::TestCase
+  end
+end
