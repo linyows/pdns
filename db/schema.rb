640913448a19cec633a5fa69d9179c95a1962543
linyows <linyows@gmail.com>
2017-05-12 12:28:09 +0900

    update mysql default schema: https://doc.powerdns.com/md/authoritative/backend-generic-mysql/

--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,76 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130429134229) do
+ActiveRecord::Schema.define(version: 20160711000000) do
   create_table 'domains', force: true do |t|
-    t.string   'name'
-    t.string   'master'
-    t.integer  'last_check'
-    t.string   'type',           default: 'NATIVE'
-    t.integer  'notified_serial'
-    t.string   'account'
-    t.integer  'ttl',            default: 86400
-    t.datetime 'created_at',     null: false
-    t.datetime 'updated_at',     null: false
-    t.integer  'user_id'
-    t.text     'notes'
+    t.string  'name',           null:  false, limit: 255
+    t.string  'master',         limit: 128
+    t.integer 'last_check'
+    t.string  'type',           null:  false, limit: 6
+    t.integer 'notified_serial'
+    t.string  'account',        limit: 40
   end
 
-  add_index 'domains', %w(name), name: 'index_domains_on_name'
+  add_index 'domains', %w(name), unique: true
 
   create_table 'records', force: true do |t|
-    t.integer  'domain_id',   null: false
-    t.string   'name',        null: false
-    t.string   'type',        null: false
-    t.string   'content',     null: false
-    t.integer  'ttl',         null: false
-    t.integer  'prio'
-    t.integer  'change_date'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+    t.integer 'domain_id'
+    t.string  'name',       limit:   255
+    t.string  'type',       limit:   10
+    t.text    'content',    limit:   64000
+    t.integer 'ttl'
+    t.integer 'prio'
+    t.integer 'change_date'
+    t.boolean 'disabled',   default: 0
+    t.binary  'ordername',  limit:   255
+    t.boolean 'auth',       default: 1
   end
 
-  add_index 'records', %w(domain_id), name: 'index_records_on_domain_id'
-  add_index 'records', %w(name type), name: 'index_records_on_name_and_type'
-  add_index 'records', %w(name), name: 'index_records_on_name'
+  add_index 'records', %w(name type)
+  add_index 'records', %w(domain_id)
+  add_index 'records', %w(domain_id ordername)
+
+  create_table 'supermasters', force: true do |t|
+    t.string 'ip',         limit: 64,  null: false
+    t.string 'nameserver', limit: 255, null: false
+    t.string 'account',    limit: 40,  null: false
+  end
+
+  create_table 'comments', force: true do |t|
+    t.integer 'domain_id',   null: false
+    t.string  'name',        null: false, limit: 255
+    t.string  'type',        null: false, limit: 10
+    t.integer 'modified_at', null: false
+    t.string  'account',     null: false, limit: 40
+    t.text    'comment',     null: false, limit: 64000
+  end
+
+  add_index 'comments', %w(domain_id)
+  add_index 'comments', %w(name type)
+  add_index 'comments', %w(domain_id modified_at)
+
+  create_table 'domainmetadata', force: true do |t|
+    t.integer 'domain_id', null: false
+    t.string  'kind',      null: false, limit: 32
+    t.text    'content'
+  end
+
+  add_index 'domainmetadata', %w(domain_id kind)
+
+  create_table 'cryptokeys', force: true do |t|
+    t.integer 'domain_id', null: false
+    t.integer 'flags',     null: false
+    t.boolean 'active'
+    t.text    'content'
+  end
+
+  add_index 'cryptokeys', %w(domain_id)
+
+  create_table 'tsigkeys', force: true do |t|
+    t.string 'name',      limit: 255
+    t.string 'algorithm', limit: 50
+    t.string 'secret',    limit: 255
+  end
+
+  add_index 'tsigkeys', %w(name algorithm), unique: true
 end
