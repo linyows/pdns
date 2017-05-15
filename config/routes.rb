26c2e9235111736ff18122a0645cb848c6e8c47a
linyows <linyows@gmail.com>
2017-05-15 19:05:43 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PDNS::Engine.routes.draw do
-  resources :domains, only: %i(create show destroy), id: PDNS.domain_format_for_routes do
-    resources :records, except: %i(index new edit), id: PDNS.domain_format_for_routes
+  resources :domains, only: %i(index create show destroy), id: PDNS.domain_format_for_routes do
+    resources :records, except: %i(new edit), id: PDNS.domain_format_for_routes
     put 'force_update_records', to: 'records#force_update_records'
   end
 end
