3354086259e82c27cf6a3070ff7025120eb22da9
linyows <linyows@gmail.com>
2017-05-23 13:09:30 +0900

    add domains routes

--- test/routes/domains_test.rb
+++ test/routes/domains_test.rb
@@ -11,5 +11,23 @@ module PDNS
       assert_recognizes({ controller: 'pdns/domains', action: 'index' }, { path: 'domains', method: :get })
       assert_routing '/domains', controller: 'pdns/domains', action: 'index'
     end
+
+    test 'domains create routes' do
+      assert_generates '/domains', controller: 'pdns/domains', action: 'create'
+      assert_recognizes({ controller: 'pdns/domains', action: 'create' }, { path: 'domains', method: :post })
+      assert_routing({ method: :post, path: '/domains' }, { controller: 'pdns/domains', action: 'create' })
+    end
+
+    test 'domains show routes' do
+      assert_generates '/domains/foo.com', controller: 'pdns/domains', action: 'show', id: 'foo.com'
+      assert_recognizes({ controller: 'pdns/domains', action: 'show', id: 'foo.com' }, { path: 'domains/foo.com', method: :get })
+      assert_routing '/domains/foo.com', controller: 'pdns/domains', action: 'show', id: 'foo.com'
+    end
+
+    test 'domains destroy routes' do
+      assert_generates '/domains/foo.com', controller: 'pdns/domains', action: 'destroy', id: 'foo.com'
+      assert_recognizes({ controller: 'pdns/domains', action: 'destroy', id: 'foo.com' }, { path: 'domains/foo.com', method: :delete })
+      assert_routing({ method: :delete, path: '/domains/foo.com' }, { controller: 'pdns/domains', action: 'destroy', id: 'foo.com' })
+    end
   end
 end
